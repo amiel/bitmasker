b0937a2d122e09aaa7666a617c4d9f4917253a7c
Amiel Martin <amiel.martin@gmail.com>
2009-10-16 14:58:06 -0700

    clean up docs

--- lib/bitmask.rb
+++ lib/bitmask.rb
@@ -24,50 +24,56 @@
 #   Bitmask.new(masks, 5) == Bitmask.new(masks, { :cat => true, :dog => false, :fish => true }) # => true
 class Bitmask
 
-  def ==(other)
-    other.masks == @masks && other.to_i == self.to_i
-  end
+	def ==(other)
+		other.masks == @masks && other.to_i == self.to_i
+	end
 
-  # create an object with which to manipulate an integer as a set of boolean values
-  # 
-  # arguments
-  # masks::  a Hash where the key is the boolean attribute and the value is the bitmask.
-  #             { :some_attribute => 0b0001, :another_attribute => 0b0010 }
-  # arg::    can be a Hash or Integer
-  def initialize(masks, arg)
-    @masks = masks
-    case arg
-    when Hash
-      @data = 0
-      arg.each do |key, value|
-        set key, value
-      end
-    else
-      @data = arg.to_i
-    end
-  end
+	# create an object with which to manipulate an integer as a set of boolean values
+	# 
+	# arguments
+	# masks::  a Hash where the key is the boolean attribute and the value is the bitmask.
+	#             { :some_attribute => 0b0001, :another_attribute => 0b0010 }
+	# arg::    can be a Hash or Integer
+	def initialize(masks, arg)
+		@masks = masks
+		case arg
+		when Hash
+			@data = 0
+			arg.each do |key, value|
+				set key, value
+			end
+		else
+			@data = arg.to_i
+		end
+	end
 
-  attr_reader :masks
+	attr_reader :masks
 
-  def to_i; @data end
-  def to_a; @masks.keys.sort{|a,b| @masks[a] <=> @masks[b] }.collect{|k| [k, get(k)]} end
-  def to_h; @masks.keys.inject({}){|h,k| h[k] = get k; h} end
+	def to_i; @data end
+	
+	def to_a
+		@masks.keys.sort{|a,b| @masks[a] <=> @masks[b] }.collect{|k| [k, get(k)] }
+	end
+	
+	def to_h
+		@masks.keys.inject({}){|h,k| h[k] = get k; h }
+	end
 
-  # def [](attr); get attr end
-  # def []=(attr, value); set attr, value end
+	# def [](attr); get attr end :nodoc:
+	# def []=(attr, value); set attr, value end :nodoc:
 
-  # returns boolean value
-  def get(attr)
-    (@data & @masks[attr]) == @masks[attr]
-  end
+	# returns boolean value
+	def get(attr)
+		(@data & @masks[attr]) == @masks[attr]
+	end
 
-  # expects a boolean value
-  def set(attr, value)
-    case value
-    when true
-      @data |=  @masks[attr]
-    when false
-      @data &= ~@masks[attr]
-    end
-  end
+	# expects a boolean value
+	def set(attr, value)
+		case value
+		when true
+			@data |=  @masks[attr]
+		when false
+			@data &= ~@masks[attr]
+		end
+	end
 end
